8d91f0ed78eb46a31c135c892643f6ac3c461c4b
Manu <manuhulolo@gmail.com>
2026-03-20 08:15:51 -0400

    Changes

--- Assets/Scripts/Enemy1.cs
+++ Assets/Scripts/Enemy1.cs
@@ -5,6 +5,7 @@ public class Enemy1 : MonoBehaviour
     public float speed = 2f;
     public float detectionRange = 5f;
     public Transform player;
+    private bool touchingPlayer = false;
 
     void Update()
     {
@@ -12,11 +13,21 @@ public class Enemy1 : MonoBehaviour
 
         float distance = Vector2.Distance(transform.position, player.position);
 
-        if (distance < detectionRange)
+        if (distance < detectionRange && !touchingPlayer)
         {
             // Move toward the player
             Vector2 direction = (player.position - transform.position).normalized;
             transform.position += (Vector3)direction * speed * Time.deltaTime;
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Player")){
+            touchingPlayer = true;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Player")) {
+            touchingPlayer = false;
         }
     }
 }
